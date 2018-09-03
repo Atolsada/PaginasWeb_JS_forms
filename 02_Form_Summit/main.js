beb6d7587187eb27064c3745d7fb9577a230c991
Atolsada <Atolsadag@gmail.com>
2018-09-03 13:40:57 +0200

    Create

---
+++ 02_Form_Summit/main.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('form').addEventListener('submit',
+        function (oEv) {
+            oEv.preventDefault() //<--- Para que no envie
+            console.log('Evento submit')}
+    )
+    /* document.querySelector('button').addEventListener('click',
+        function () {console.log('Evento click')}
+    ) 
+    Para que veamos que con click se puede hacer
+    */
+})
