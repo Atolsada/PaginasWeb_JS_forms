7bcb3ed6659a447bbe9a5d2326533ae600008d1e
Atolsada <Atolsadag@gmail.com>
2018-09-03 10:39:55 +0200

    Inicio, explicacion clases - funciones - etc

---
+++ 01_Inicio/js/app.js
@@ -0,0 +1,40 @@
+// JS es un lenguaje orientado a eventos
+
+// funcion manejadora de eventos clave (ejem)- esperar a que se termine de cargar todo antes de iniciar los eventos
+
+/* function iniciar(){
+    console.log("Iniciado JS")
+} */
+
+// Funcion con nombre
+//document.addEventListener('DOMContentLoaded', iniciar)
+
+// Si ponemos sólo
+//iniciar() <---Lo mismo que arriba pero la invocamos nosotros y no el explorador/sistema con addEventListener
+
+// Funcion anónima
+//document.addEventListener('DOMContentLoaded', function () {
+//    console.log("Iniciado JS, función anónima")
+//})
+
+// Funcion arrow
+/* document.addEventListener('DOMContentLoaded', () =>
+    console.log("Iniciado JS, función anónima")
+) */
+
+// Funcion más purista (se invoca a si misma) en cualquier formato: anonima, arrow, nombre
+//(() => 
+//    document.addEventListener('DOMContentLoaded',  () => 
+//        console.log("Iniciado JS, función anónima")
+//    ) 
+//)()
+
+
+import { Main} from './main.js'
+
+// document.addEventListener('DOMContentLoaded', () => new Main())
+
+//Igual que arriba (son anonimas) pero arriba es mas compacta
+document.addEventListener('DOMContentLoaded', 
+   function () {new Main()
+   })
