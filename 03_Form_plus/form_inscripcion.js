002f2cb49562f04b2b45a2f19be61443c3576454
Atolsada <Atolsadag@gmail.com>
2018-09-04 13:26:11 +0200

    Fixed teacher

--- 03_Form_plus/form_inscripcion.js
+++ 03_Form_plus/form_inscripcion.js
@@ -1,6 +1,9 @@
 import { FormularioModel} from './model.js'
-export class FormInscripcion {
+import { Form } from './form.js'
+
+export class FormInscripcion extends Form {
     constructor() {
+        super() //<--- En un extends se pone super siempre
         this.domForm = document.querySelector('form')
         this.data = new FormularioModel()
         this.domForm.addEventListener('submit',
@@ -8,7 +11,7 @@ export class FormInscripcion {
         ) 
         console.log(this.data)
     }
-    
+
     enviar(oEv) {
         oEv.preventDefault()
         this.data.user.name = document.querySelector('#nombre').value
@@ -19,73 +22,10 @@ export class FormInscripcion {
         this.data.coments = document.querySelector('#comentarios').value
         this.data.opciones.conditions = document.querySelector('#condiciones').checked
         this.data.opciones.turn = this.setRadio('turno')
-        this.data.opciones.curse = this.setCurso() 
-        this.data.aficiones = this.setAficiones()
+        this.data.opciones.curse = this.setSelect('curso') 
+        this.data.aficiones = this.setMultiCheck('aficiones')
         console.log(this.data)
         //console.dir(this.domForm)
-        
     }
 
-    /* Procesa un radioButton */
-    // La hacemos funcion helper
-
-    setRadio(name) {
-        // let aTurnos = document.getElementsByName('turno')
-
-        //let id = '[name=' + name + ']'
-        const id = `[name='${name}']`
-        const aRadio = document.querySelectorAll(id)
-        for (let i = 0; i < aRadio.length; i++) {
-            const item = aRadio[i];
-            if (item.checked) {
-                return item.value
-            }
-        }
-        
-        /* let r = ''
-
-        let aDatos = []
-        aTurnos.forEach(
-            item => aDatos.push(item)
-        )
-        aDatos.some(    
-            (item, i) => {
-                console.log("Vuelta", i+1)
-                if (item.checked) {
-                    r = item.value
-                    return true
-                }
-            })
-        return r */
-    }
-
-     /* enviar(oEv) {
-        oEv.preventDefault()
-        //Dos formas de recoger los datos <----
-        this.data.user = this.domUser.value
-        this.dara.passw = document.querySelector('#apassw').value //<--- Tambien se puede poner asi ( la otra forma la de this.data.user)
-        console.dir(this.data)
-    } */
-
-    setCurso() {
-        const curso = document.querySelector('#curso')
-        console.dir(curso.selectedIndex)
-        console.dir(curso[curso.selectedIndex])
-        const oDato = {
-            value :  curso[curso.selectedIndex].value,
-            text :  curso[curso.selectedIndex].textContent
-        }
-        return oDato
-    }
-
-    setAficiones() {
-        let aDatos = []
-        let aAficiones = document.querySelectorAll('.aficiones')
-        aAficiones.forEach( item => {
-            if (item.checked) {
-                aDatos.push(item.id)
-            }
-        })
-        return aDatos
-    }
 }
--- 03_Form_plus/main.js
+++ 03_Form_plus/main.js
@@ -1,4 +1,4 @@
-import { FormInscripcion } from './form_inscripcion'
+import { FormInscripcion } from './form_inscripcion.js'
 
 document.addEventListener('DOMContentLoaded', function () {
     new FormInscripcion()
