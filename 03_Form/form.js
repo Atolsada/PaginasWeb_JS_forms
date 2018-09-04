8dee02a9f0c8ee9c8d0d66d3ff686cbf6cdecc91
Atolsada <Atolsadag@gmail.com>
2018-09-04 10:05:47 +0200

    L17 -this.domForm.elements.nombre - Otra forma de hacer a traves de elements

--- 03_Form/form.js
+++ 03_Form/form.js
@@ -1,19 +1,38 @@
 export class Form {
     constructor() {
         this.domForm = document.querySelector('form')
-        this.domUser = document.querySelector('#user')
         this.data = {
-            user = '',
-            passw = ''
+            opciones : {
+                conditions: '',
+                turn: '',
+                curse: ''
+            },
+            user : { 
+                name: '', 
+                f_name: ''},
+            email: '',
+            passw : ''
         }
         this.domForm.addEventListener('submit',
-            this.enviar.bind(this)
+            this.enviar.bind(this.domForm.elements.nombre)
         ) 
     }
+
     enviar(oEv) {
         oEv.preventDefault()
-        this.data.user = this.domUser.value
-        this.dara.passw = document.querySelector('#apassw').value //<--- Tambien se puede poner asi
-        console.dir(this.data)
+        // this.data.user.name = document.querySelector('#nombre').value
+        // this.data.passw = document.querySelector('#passw').value
+        // console.log(this.data)
+        
     }
 }
+
+
+/*     enviar(oEv) {
+        oEv.preventDefault()
+        //Dos formas de recoger los datos <----
+        this.data.user = this.domUser.value
+        this.dara.passw = document.querySelector('#apassw').value //<--- Tambien se puede poner asi ( la otra forma la de this.data.user)
+        console.dir(this.data)
+    }
+} */
