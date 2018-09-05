abf1e795a0d41eb587c568c6ea9f2e40c099008b
Atolsada <Atolsadag@gmail.com>
2018-09-05 13:41:51 +0200

    End y ver si fix

--- 04_Interactivo/app.js
+++ 04_Interactivo/app.js
@@ -1,20 +1,93 @@
+import { GENEROS } from './datos.js'
 export class App {
     constructor() {
         this.dom = {
             formInicio : document.querySelector('#formInicio'),
-            formFinal : document.querySelector('formFinal'),
-            cboxMas : document.querySelector('#mas')
+            formFinal : document.querySelector('#formFinal'),
+            cbMas : document.querySelector('#mas'),
+            slGenero : document.querySelector('#genero'),
+            slAutores : document.querySelector('#autor'),
+            btnEnviar : document.querySelector('#btnEnviar'),
+            /* btnraro : document.querySelector('#btnraro') */
         }
-    this.defineHandlers()
+        this.aGeneros = GENEROS
+        this.generosFirst = true
+        this.defineHandlers()
+        this.crearSelect(this.aGeneros, this.dom.slGenero)
     }
 
     defineHandlers () {
-        this.dom.cboxMas.addEventListener('change',
-        this.setFormFinal.bind(this)) // bind(this) para que funcione en el this.dom.formFinal
-    }
+        this.dom.cbMas.addEventListener('change',
+            this.setFormFinal.bind(this)) // bind(this) para que funcione en el this.dom.formFinal
+        this.dom.slGenero.addEventListener('change',
+            this.changeGenero.bind(this))
+        this.dom.formFinal.addEventListener('submit',
+            this.enviar.bind(this))
+        /* this.dom.btnraro.addEventListener('click',
+            this.enviarRaro.bind(this)) */
+}
 
     setFormFinal () {
         //console.log('Cambio') <--- vemos el "Cambio" de que funciona el checkbox en la consola
         this.dom.formFinal.classList.toggle('hide')
     }
+
+        /* const aGeneros = [
+            {value:'', label: ''},
+            {value:'P', label: 'Policiaco'},
+            {value:'SF', label: 'Ciencia Ficcion'},
+            {value:'F', label: 'Fantasia'}
+        ]
+ */
+    crearSelect(aDatos, target, selected) {
+        let html = ''
+        aDatos.forEach( (item, i) => {
+            if( i === selected) {
+
+            } else {
+                html += `<option value="${item.value}"selected>
+                        ${item.label}</option>`  
+            }
+        })
+        target.innerHTML =  html
+    }
+
+        /*this.dom.sGenero.innerHTML =
+            `<option value="P">Policiaco</option>
+            <option value="SF">Ciencia Ficcion</option>
+            <option value="F">Fantasia</option>` */
+
+    changeGenero() {
+        let i = this.dom.slGenero.selectedIndex
+        console.log( 'Seleccionado', i )
+        if (this.generosFirst) {
+            this.aGeneros.shift()
+            this.crearSelect(this.aGeneros, this.dom.slGenero, i-1)
+            this.generosFirst = false
+            this.dom.btnEnviar.disabled = false
+        }
+        i = this.dom.slGenero.selectedIndex
+        console.dir( i )
+        console.dir( this.dom.slGenero[i])
+        console.log(this.aGeneros[i].autores)
+        this.dom.slAutores.disabled = false
+        this.crearSelect(this.aGeneros[i].autores, this.dom.slAutores)
+    }
+        
+    enviar(oEv) {
+        //oEv.preventDefault()
+        const data = '?genero=' + 
+        this.dom.slGenero[this.dom.slGenero.selectedIndex].value
+        + '&autor=' +
+        this.dom.slAutores[this.dom.slAutores.selectedIndex].value
+        console.log(data)
+    }
+
+    /* enviarRaro(){
+        this.dom.formFinal.submit()
+    } */
 }
+
+        
+        
+
---
+++ 04_Interactivo/datos.js
@@ -0,0 +1,18 @@
+export const GENEROS = [
+    {value:'', label: ''},
+    {value:'P', label: 'Policiaco', autores : [
+        {value:'Agata Christie', label: 'Agata Christie'},
+        {value:'Georges Simenon', label: 'Georges Simenon'},
+        {value:'Arthur C. Doyle', label: 'Arthur C. Doyle'},
+    ]},
+    {value:'SF', label: 'Ciencia Ficcion', autores: [
+        {value:'Isaac Asimov', label: 'Isaac Asimov'},
+        {value:'William Gibson', label: 'William Gibson'},
+        {value:'Bradbury', label: 'Bradbury'},
+    ]},
+    {value:'F', label: 'Fantasia', autores: [
+        {value:'Terry Pratchett', label: 'Terry Pratchett'},
+        {value:'JRR Tolkien', label: 'JRR Tolkien'},
+        {value:'Bran Stroker', label: 'Bran Stroker'},
+    ]}
+]
