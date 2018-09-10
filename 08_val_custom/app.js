004c9ee668af3a48037ecd075aeb8727d6ae1f4a
Atolsada <Atolsadag@gmail.com>
2018-09-10 11:16:33 +0200

    Añadiendo funcion de DNI - need fix

--- 08_val_custom/app.js
+++ 08_val_custom/app.js
@@ -6,7 +6,7 @@ function main () {
     inDni.setCustomValidity('DNI Incorrecto') //setCustomValidity aquí por que en html no existe como atributo
 
     form.addEventListener('submit', enviar)
-    btnEnviar.addEventListener('click', validaDNI)
+    btnEnviar.addEventListener('input', validaDNI)
 
     function enviar(oE){ //objetoEvento
 
@@ -17,12 +17,24 @@ function main () {
         
     function validaDNI() {
         console.log('Validando DNI')
-        if (inDNI.value){
-            inDni.setCustomValidity('')
-        }
+        const LETRAS = 'TRWAGMYFPDXBNJZSQVHLCKET'
+        let num = inDni.value.substring(0, inDni.value.length-1)
+        let letra =  inDni.value.substring(inDni.value.length-1)
 
+        if (isNaN(num *1) || num < 1 || num > 99999999 ) {
+            return 'Error'
+        } else if(LETRAS[num%23] !== letra) {
+            return 'Error'
+        }
+ 
+            //inDni.setCustomValidity('')
     }
     
+    /* function letraDNI(num, letra) {
+        const LETRAS = 'TRWAGMYFPDXBNJZSQVHLCKET'
+        
+        return (LETRAS[num%23]=== letra) // es funcion boolean por estos parentesis, evalua el true o false correspondiente
+    } */
 }
 
 
