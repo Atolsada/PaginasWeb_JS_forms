e8fd7d9791053acdd4f06923d0716a24f76b592d
Atolsada <Atolsadag@gmail.com>
2018-09-10 13:55:05 +0200

    Ultimo minuto no se ve bien - Maybe fix

--- 08_val_custom/app.js
+++ 08_val_custom/app.js
@@ -1,12 +1,19 @@
 function main () {
-    form = document.querySelector('form')
-    inDni = document.querySelector('#dni')  
-    btnEnviar = document.querySelector('#btnEnviar')
+    let form = document.querySelector('form')
+    let inDni = document.querySelector('#dni')
+    let inUser = document.querySelector('#user')
+    let btnEnviar = document.querySelector('#btnEnviar')
 
-    inDni.setCustomValidity('DNI Incorrecto') //setCustomValidity aquí por que en html no existe como atributo
+    let inPass1 = document.querySelector('#passw')
+    let inPass2 = document.querySelector('#passw2')
+
+    inUser.setCustomValidity('Nombre de usuario imprescindible')
 
     form.addEventListener('submit', enviar)
-    btnEnviar.addEventListener('input', validaDNI)
+    inDni.addEventListener('input', validaDNI)
+    inUser.addEventListener('input', validaUser)
+
+    inPass2.addEventListener('input', compararPassw)
 
     function enviar(oE){ //objetoEvento
 
@@ -16,18 +23,20 @@ function main () {
     }
         
     function validaDNI() {
+        let msgDNI = 'DNI Incorrecto'
+        inDni.setCustomValidity(msgDNI)
         console.log('Validando DNI')
         const LETRAS = 'TRWAGMYFPDXBNJZSQVHLCKET'
         let num = inDni.value.substring(0, inDni.value.length-1)
         let letra =  inDni.value.substring(inDni.value.length-1)
-
+        console.log(num, letra)
+        console.dir(inDni)
         if (isNaN(num *1) || num < 1 || num > 99999999 ) {
-            return 'Error'
+            return false
         } else if(LETRAS[num%23] !== letra) {
-            return 'Error'
+            return false
         }
- 
-            //inDni.setCustomValidity('')
+        inDni.setCustomValidity('')
     }
     
     /* function letraDNI(num, letra) {
@@ -35,6 +44,21 @@ function main () {
         
         return (LETRAS[num%23]=== letra) // es funcion boolean por estos parentesis, evalua el true o false correspondiente
     } */
+
+    function validaUser() {
+        inUser.setCustomValidity('Nombre de usuario imprescindible')
+        if (inUser) {
+            inUser.setCustomValidity('')
+        }
+    }
+
+    function compararPassw() {
+        let msg = "";            
+        if (inPass1.value != inPass2.value) {
+            msg = "Las contraseñas no coinciden" 
+        }
+        inPass2.setCustomValidity(msg)
+    }
 }
 
 
