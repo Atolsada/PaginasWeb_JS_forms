e5e0fcec65d51c9aeda6bb4f5a6ba6d05c2c752f
Atolsada <Atolsadag@gmail.com>
2018-09-11 12:22:32 +0200

    Copy AJAX plus desde basico

---
+++ 10_AJAX_Plus/app.js
@@ -0,0 +1,58 @@
+import { FILE } from './config.js'
+
+function main () {
+    document.querySelector('#btnDatos')
+    .addEventListener('click', pedirDatos)
+            
+    /* document.querySelector('#btnDatos')
+    .onclick = pedirDatos */
+
+    function pedirDatos() {
+        //console.log('Pidiendo datos')
+        console.dir(FILE)
+    
+        let ajax = new XMLHttpRequest()
+        console.dir(ajax)
+        ajax.onreadystatechange = mostrarDatos
+
+        //ajax.open() prepara la peticion(abre el canal de comunicaciones) get-post-put-delete
+        //ajax.send() lanza la operacion
+        ajax.open('GET', 'datos.json')
+        ajax.send(null)
+
+        function mostrarDatos(){
+            console.log(ajax.readyState) //4
+
+            let oDatos
+            let html
+
+            if(ajax.readyState === 4){
+                console.log(ajax.status) //200 bien 404 mal
+                if(ajax.status === 200){
+                    oDatos = JSON.parse(ajax.responseText)//responseText es un string por que ajax solo se envian strings
+                    console.dir(oDatos) 
+                    html = '<ul>'
+
+                    for (const key in oDatos) {
+                        if (oDatos.hasOwnProperty(key)) {
+                            const value = oDatos[key];
+                            html += `<li>${key} : ${value}</li>`
+                        }
+                    }
+                    html += '</ul>'
+
+                    document.querySelector('#output')
+                    .innerHTML = ajax.responseText
+                } else {
+                    document.querySelector('#output')
+                    innerHTML = ajax.statusText + ' : ' +
+                    ajax.statusText
+                }
+
+            }
+        }
+
+    }
+}
+
+document.addEventListener('DOMContentLoaded', main)
