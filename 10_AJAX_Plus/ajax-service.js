a085478ae7b7eaadb6f96befaae003f8a9b3dc3c
Atolsada <Atolsadag@gmail.com>
2018-09-12 10:30:22 +0200

    end

--- 10_AJAX_Plus/ajax-service.js
+++ 10_AJAX_Plus/ajax-service.js
@@ -13,7 +13,8 @@ export class AjaxService {
         this.ajax.addEventListener('readystatechange',
             this.getDatos.bind(this))
 
-        this.ajax.open(metodo,url)
+        this.ajax.open(metodo,url, false, 'pepe','1234')
+        this.ajax.setRequestHeader('Accept', 'text/txt')
         this.ajax.send(null)
     }
 
@@ -21,6 +22,7 @@ export class AjaxService {
         console.log(this.ajax.readyState)
         if(this.ajax.readyState === 4) {
             console.log(this.ajax.status)
+            console.dir(this.ajax.getAllResponseHeader())
             if(this.ajax.status === 200) {
                 if (this.type === 'json') {
                     this.oDatos = 
