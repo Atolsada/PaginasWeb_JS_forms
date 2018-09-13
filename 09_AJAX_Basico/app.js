e41d6b19c57dc6fef5ad60160fc276883d2d83d6
Atolsada <Atolsadag@gmail.com>
2018-09-13 10:13:17 +0200

    I think is fixed now

--- 09_AJAX_Basico/app.js
+++ 09_AJAX_Basico/app.js
@@ -3,9 +3,11 @@ import { FILE } from './config.js'
 function main () {
     document.querySelector('#btnDatos')
     .addEventListener('click', pedirDatos)
-            
+
     /* document.querySelector('#btnDatos')
-    .onclick = pedirDatos */
+        .onclick = pedirDatos*/
+
+
 
     function pedirDatos() {
         //console.log('Pidiendo datos')
@@ -17,38 +19,36 @@ function main () {
 
         //ajax.open() prepara la peticion(abre el canal de comunicaciones) get-post-put-delete
         //ajax.send() lanza la operacion
-        ajax.open('GET', 'datos.json')
+        ajax.open('GET','datos.json')
         ajax.send(null)
 
-        function mostrarDatos(){
+        function mostrarDatos() {
             console.log(ajax.readyState) //4
 
             let oDatos
             let html
 
-            if(ajax.readyState === 4){
+            if(ajax.readyState === 4) {
                 console.log(ajax.status) //200 bien 404 mal
-                if(ajax.status === 200){
+                if(ajax.status === 200) {
                     oDatos = JSON.parse(ajax.responseText)//responseText es un string por que ajax solo se envian strings
                     console.dir(oDatos) 
                     html = '<ul>'
-
                     for (const key in oDatos) {
                         if (oDatos.hasOwnProperty(key)) {
                             const value = oDatos[key];
-                            html += `<li>${key} : ${value}</li>`
+                            html += `<li>${key} : ${value}</li>`    
                         }
                     }
                     html += '</ul>'
-
                     document.querySelector('#output')
                     .innerHTML = html
                 } else {
                     document.querySelector('#output')
-                    innerHTML = ajax.statusText + ' : ' +
+                    .innerHTML = ajax.status + ' : ' +
                     ajax.statusText
                 }
-
+                 
             }
         }
 
