0e89080e0232e78e68977d2664093f59f4fd4faa
Atolsada <Atolsadag@gmail.com>
2018-09-13 09:53:26 +0200

    fin?

--- 11_Promesas/promesas.js
+++ 11_Promesas/promesas.js
@@ -2,7 +2,7 @@ function main() {
 
     function promesas() { //<--- Este es el burguer (new promise, el NEW instancia)
         return new Promise( //<--- Este es el ticket de compra de la hamburguesa (suele encontrarse en las primeras lineas)
-            (resolve) => {
+            (resolve) => { 
                 setTimeout (() => resolve('Ha pasado un rato'), 3000) //<--- Lo que tarda en hacerse la hamburguesa ('la hamburguesa')
             }
         )
@@ -12,7 +12,7 @@ function main() {
     /* EJEMPLO POR SI NO QUEDA CLARO */
 
     function promesa2() { //return new Promise( (resolve, reject) => {}) funciones - resolve resuelve, reject por si algo salio mal
-        return new Promise( (resolve, reject) => {
+        return new Promise( (resolve, reject) => { //(resolve, reject) si se que nunca va a haver error no ponemos reject
             //TO DO
             setTimeout ( ()=>{
                 // TO DO
