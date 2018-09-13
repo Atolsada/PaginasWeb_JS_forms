7b48300f159d3857ab28367802ea4085e65ad4a8
Atolsada <Atolsadag@gmail.com>
2018-09-13 09:42:49 +0200

    anotaciones para entender promise

--- 11_Promesas/promesas.js
+++ 11_Promesas/promesas.js
@@ -1,11 +1,29 @@
 function main() {
 
-    function promesas() {
-        return new Promise(
+    function promesas() { //<--- Este es el burguer (new promise, el NEW instancia)
+        return new Promise( //<--- Este es el ticket de compra de la hamburguesa (suele encontrarse en las primeras lineas)
             (resolve) => {
-                setTimeout (() => resolve('Ha pasado un rato'), 3000)
+                setTimeout (() => resolve('Ha pasado un rato'), 3000) //<--- Lo que tarda en hacerse la hamburguesa ('la hamburguesa')
             }
         )
+    }
+
+    function promesa2() { //return new Promise( (resolve, reject) => {}) funciones - resolve resuelve, reject por si algo salio mal
+        return new Promise( (resolve, reject) => {
+            //TO DO
+            setTimeout ( ()=>{
+                // TO DO
+                let comida = 'comida preparada'
+                if (comida) {
+                    resolve(comida) //<--- avisa a la persona y va a dar la comida
+                }else{
+                    reject('Error')
+                }
+
+                
+            }, 3000)
+        })
+
     }
 
     /* function pintar(msg) {
@@ -15,7 +33,7 @@ function main() {
     promesas()
     .then(pintar */
 
-    promesas().then(
+    promesas().then( //<--- Es el señor que te atiende en el burguer 
         (response) => { console.log(response)}
     )
 
