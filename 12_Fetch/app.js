f39bc6928ce03cbc8903de62d17a8bfda40275b4
Atolsada <Atolsadag@gmail.com>
2018-09-13 11:19:27 +0200

    Fixit

--- 12_Fetch/app.js
+++ 12_Fetch/app.js
@@ -24,43 +24,32 @@ export class App {
     pedirDatos(oEv) {
         switch (oEv.target.id) {
             case 'btnDatos':
-                fetch(DATOS,{method: 'GET'}).then(
+                fetch(DATOS, {method:'GET'}).then(
                     (response) => {
                         console.dir(response)
                         return response.text()
-                    } 
+                    }
                 ).then(
-                    (response) => {
+                    (response) => { 
                         console.log(response)
                         this.mostrarDatos({respuesta: response})
                     }
                 )
-                /* new AjaxService('GET', DATOS, 'txt',  
-                    this.mostrarDatos.bind(this)) */
                 break;
             case 'btnJson':
-                    fetch(JSON, {method: 'GET'}).then(
-                        (response) => {
-                            return response.json()
-                        }
-                    ).then(
-                        (data) => {
-                            console.dir(data)
-                            this.mostrarDatos(data)
-                        }
-                    )
-                /* new AjaxService('GET', JSON , 'json', 
-                    this.mostrarDatos.bind(this)) */
+                fetch(JSON, {method: 'GET'}).then(
+                    (response) => { return response.json()}
+                ).then(
+                    (data) => {this.mostrarDatos(data)}
+                )
                 break;  
-            case 'btnError':
-                        fetch('error', {method: 'GET'}).then(
-                            (response) => {
-                                console.log(response)
-                                this.mostrarError(response)
-                            }
-                        )
-                /* new AjaxService('GET', 'error', '', 
-                    this.mostrarError.bind(this)) */
+            case 'btnError':  
+                fetch('error', {method: 'GET'}).then(
+                    (response) => {
+                        console.log(response)
+                        this.mostrarError(response) 
+                    }
+                )
             break;                   
         }
     } 
@@ -80,9 +69,10 @@ export class App {
     }
 
     mostrarError(oDatos) {
-        this.ndOutput.innerHTML = ''
-        /* this.ndError.innerHTML = oDatos.error */
-        this.ndError.innerHTML = error
         let error = oDatos.status + ' : ' + oDatos.statusText
+        this.ndError.innerHTML = error
+        this.ndOutput.innerHTML = ''
     }
 }
+
+
