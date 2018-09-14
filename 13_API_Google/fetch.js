54d66491fbd64120341c5761892903679854ff2f
Atolsada <Atolsadag@gmail.com>
2018-09-14 11:18:30 +0200

    ejemplo fetch de errores

---
+++ 13_API_Google/fetch.js
@@ -0,0 +1,20 @@
+//trycach
+
+function hacerAlgo(){
+    if('Hay un error de la leche'){ //throw <-- genera errores
+        //throw   To do gestion del error
+        throw 'error' //si sale el error lo gestiona el catch
+        }else {
+            // To do codigo de la funcionalidad
+            return x //si sale bien lo gestiona el hacerAlgo del try
+        }
+}
+
+ try {
+    hacerAlgo()
+    } catch (error) {
+        hacer_otra_cosa()
+}
+
+
+
