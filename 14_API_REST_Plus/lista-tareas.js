d2970974eceec1d837c75c6ba71225eb96c4fdf3
Atolsada <Atolsadag@gmail.com>
2018-09-20 13:16:45 +0200

    medium fixed

--- 14_API_REST_Plus/lista-tareas.js
+++ 14_API_REST_Plus/lista-tareas.js
@@ -6,30 +6,26 @@ export class ListaTareas {
         this.uRL = 'http://localhost:3000/tareas'
         this.aTareas = []
         this.nodoListaTareas = document.querySelector('#lista')
+        this.fetchService = new FetchService()
         this.getTareas()
     }
 
     getTareas() {
-        new FetchService({url:this.uRL, method: 'GET' })
-            .get()
+        this.fetchService.send(this.uRL, {method: 'GET' })
             .then( data => {
                 this.aTareas = data
+                console.log("Ejecutando getTareas")   
                 this.renderLista()
             },
-            (error) =>{console.dir(error)}
+            error => {console.dir(error)}
             )
     }
     
     renderLista() {
-        /* let html = ''
-        this.aTareas.forEach( 
-            item => html += new Tarea(item).renderTarea()
-        )
-        this.nodoListaTareas.innerHTML = html */
-
         this.aTareas.forEach( 
             item => {
-             this.nodoListaTareas.appendChild(new Tarea(item).renderTarea())   
+             this.nodoListaTareas.appendChild(new Tarea(item).getTarea())   
+             //this.nodoListaTareas.appendChild(new Tarea(item).nodoTarea )   
             }
         )
     }
--- 14_API_REST_Plus/tarea.js
+++ 14_API_REST_Plus/tarea.js
@@ -1,42 +1,35 @@
 export class Tarea {
 
     constructor(data) {
+        this.id = data.id
         this.name = data.name
         this.isComplete = data.isComplete
-
         this.nodoTarea = ''
         this.crearTarea()
+        this.nodoCheck = this.nodoTarea.querySelector('input')
+        this.nodoBorrar = this.nodoTarea.querySelector('.borrar-tarea')
 
-        //this.nodoCheck = document.querySelector('#isCompleta')
-        //this.nodoBorrar = document.querySelector('#btnBorrar')
+        this.nodoCheck.addEventListener('change', this.sendChecked.bind(this))
+        this.nodoBorrar.addEventListener('click', this.sendBorrar.bind(this))
 
-        this.nodoCheck = this.nodoTarea.querySelector('#isCompleta')
-        this.nodoBorrar = this.nodoTarea.querySelector('#btnBorrar')
-
-        console.log(this.nodoCheck, this.nodoBorrar)
+        //console.log(this.nodoCheck, this.nodoBorrar )
     }
 
-    crearTarea(){
+    crearTarea() {
         this.nodoTarea = document.createElement('li')
-        //console.dir(tarea)
         this.nodoTarea.innerHTML=`
-            <input type="checkbox" name="isCompleta" id="isCompleta"
-                ${this.isComplete ? 'checked' : '' }>
-            <span class="nombreTarea">${this.name}</span>
-            <span id="btnBorrar" class="borrarTarea">🗑️</span>
+            <input type="checkbox" name="is-completa" id="check-${this.id}"
+                data-id="${this.id}" ${this.isComplete ? 'checked' : '' }>
+            <span class="nombre-tarea ${this.isComplete ? 'hecho' : '' }">
+                ${this.name}</span>
+            <span id="borrar-${this.id}" data-id="${this.id}"
+                class="borrar-tarea ${this.isComplete ? 'borrar-activo' : 'inactivo' }">🗑️</span>
         `
         console.dir(this.nodoTarea)
     }
 
-    renderTarea() {
-        //let htmlView =  `
-        //    <li>
-        //    <input type="checkbox" name="isCompleta" id="isCompleta"
-        //        ${this.isComplete ? 'checked' : '' }>
-        //    <span class="nombreTarea">${this.name}</span>
-        //    <span id="btnBorrar"class="borrarTarea">🗑️</span>
-        //    </li>
-        //` 
-        //return htmlView
+
+    getTarea() {
+        return this.nodoTarea
     }
 }
